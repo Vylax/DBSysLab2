740f110e002149377aa04e16a05d597cb59abf86
Jordan Allemand <vylaxez@gmail.com>
2022-12-09 11:32:09 +0100

    created HeapFileIterator class to implement HeapFile.iterator() method

--- src/java/simpledb/HeapFile.java
+++ src/java/simpledb/HeapFile.java
@@ -122,8 +122,8 @@ public class HeapFile implements DbFile {
 
     // see DbFile.java for javadocs
     public DbFileIterator iterator(TransactionId tid) {//CHANGES
-        // some code goes here
-        return null;
+        
+        return new HeapFileIterator(f,td,id, tid);
     }
 
 }
